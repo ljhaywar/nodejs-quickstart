8cb3318c5acab7649f3a2d5c79746ab931197bd7
Lauren Schaefer <lauren.schaefer@mongodb.com>
2019-10-08 08:13:16 -0400

    Update the code based on blog post review

--- connection.js
+++ connection.js
@@ -1,51 +1,45 @@
 const {MongoClient} = require('mongodb');
 
-// Connection URI. Update <username>, <password>, and <your-cluster-url> to reflect your cluster.
-// See https://docs.mongodb.com/ecosystem/drivers/node/ for more details
+/**
+ * Connection URI. Update <username>, <password>, and <your-cluster-url> to reflect your cluster.
+ * See https://docs.mongodb.com/ecosystem/drivers/node/ for more details
+ */
 const uri = "mongodb+srv://<username>:<password>@<your-cluster-url>/test?retryWrites=true&w=majority";
 
-// The Mongo Client you will use to interact with your database
-// See https://mongodb.github.io/node-mongodb-native/3.3/api/MongoClient.html for more details
-const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+async function main(){
+    /**
+     * The Mongo Client you will use to interact with your database
+     * See https://mongodb.github.io/node-mongodb-native/3.3/api/MongoClient.html for more details
+     */
+    const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
 
-// Connect to the database
-client.connect(makeDbCalls);
-
-/**
- * Handle any DB connection errors, make DB calls, and close the DB connection
- * @param {Error} err 
- */
-async function makeDbCalls(err){
-    // Handle any DB connection errors
-    if(err){
-        console.error(`An error occurred while connecting to the MongoClient: ${err}`);
-        return;
-    } else {
-      console.log("Connected successfully to the server");
-    } 
-  
-    // Access the listingsAndReviews collection that is stored in the sample_airbnb DB
-    let collection = client.db("sample_airbnb").collection("listingsAndReviews");
-
-    // Make the appropriate DB calls
     try {
+        // Connect to the MongoDB cluster
+        await client.connect();
+
+        // Access the listingsAndReviews collection that is stored in the sample_airbnb DB
+        let collection = client.db("sample_airbnb").collection("listingsAndReviews");
+
+        // Make the appropriate DB calls
         await printFiveListings(collection);
+
     } catch (e) {
-        console.error(`Unable to print listings: ${e}`);
-        return;
+        console.error(e);
+    } finally {
+        await client.close();
     }
-  
-    // Close the DB connection
-    await client.close();
 }
+
+main().catch(console.err);
 
 /**
  * Print the names of five Airbnb listings
  * @param {Collection} collection The collection to search
  */
 async function printFiveListings(collection){
-    const results = await collection.find({}).limit(5).toArray();
+    let cursor = await collection.find({}).limit(5);
+    let docs = await cursor.toArray();
 
     console.log("Found Airbnb listings in the database:");
-    results.forEach(result => console.log(` - ${result.name}`));
+    docs.forEach(doc => console.log(` - ${doc.name}`));
 };
